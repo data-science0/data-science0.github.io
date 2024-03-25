029f338f269e902d279b03c2d882f5e8534f8807
MirzaOsama09 <mirza.osama96@gmail.com>
2024-03-25 13:59:34 +0500

    testingCaptcha

--- python-funnel/js/mail.js
+++ python-funnel/js/mail.js
@@ -1,14 +1,39 @@
-function sendMail(){
+function sendMail() {
+
     var params = {
         name: document.getElementById('NameUser').value,
         email: document.getElementById('EmailUser').value,
         contact: document.getElementById('ContactUser').value
     };
-    var serviceId = 'service_uxdslki';
-    var templateId = 'template_qots349';
 
-    emailjs.send(serviceId, templateId, params).then((res) => {
-        console.log(res);
-    })
-    .catch((error) => console.log(error));
+    function validateEmail(email) {
+        // Regular expression for basic email validation
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(email);
+    }
+
+    console.log(params);
+
+    grecaptcha.ready(function() {
+        grecaptcha.execute('reCAPTCHA_site_key', {action: 'submit'}).then(function(token) {
+            // Add your logic to submit to your backend server here.
+            console.log(token +  ' .... ');
+        });
+      });
+
+    // if (validateEmail(params.email)) {
+    //     var serviceId = 'service_uxdslki';
+    //     var templateId = 'template_qots349';
+
+    //     emailjs.send(serviceId, templateId, params)
+    //         .then((res) => {
+    //             console.log(res);
+    //             window.location.href = 'https://lms.ustadam.org/courses/python-master-class';
+    //         })
+    //         .catch((error) => console.log(error));
+    // }
+    // else {
+    //     document.getElementById('EmailUser').focus();
+    // }
+
 }
