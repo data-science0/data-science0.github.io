6fc512afd995eb5cdc25d94dfc5f4683ff0745aa
MirzaOsama09 <mirza.osama96@gmail.com>
2024-03-25 22:14:26 +0500

    testCaptchaFunc

--- python-funnel/js/mail.js
+++ python-funnel/js/mail.js
@@ -6,39 +6,37 @@ function sendMail() {
         contact: document.getElementById('ContactUser').value
     };
 
-    function validateEmail(email) {
-        // Regular expression for basic email validation
-        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return re.test(email);
-    }
-
     console.log(params);
 
     grecaptcha.ready(function() {
-        grecaptcha.execute('6LdRv6MpAAAAAOaNpmlgZJd-V2zBIyfsBdIrgdkB', {action: 'submit'}).then(function(token) {
+        grecaptcha.execute('6LdRv6MpAAAAAOaNpmlgZJd-V2zBIyfsBdIrgdkB', {action: 'submit'}).then(async function(token) {
             // Add your logic to submit to your backend server here.
             console.log(token +  ' .... ');
-            $.ajax({
-                type: "POST",
-                url: 'https://www.google.com/recaptcha/api/siteverify',
-                headers: {
-                    'Access-Control-Allow-Origin': '*',
-                    'Content-Type': 'application/json'
-                },
-                data: {
-                    "secret": "6LdRv6MpAAAAAHknp3Ni6dnsn4HFAU9eAWH8GbJ2",
-                    "token" : token,
-                },
-                success: function(data) {
-                    if(data.response.success) {
-                        window.recaptchaScore = data.response.score;
-                        console.log('user score ' + data.response.score)
-                    }
-                },
-                error: function() {
-                    console.log('error while getting google recaptcha score!')
-                }
-            });
+
+            const verificationResult = await verifyRecaptcha(token);
+            console.log(verificationResult);
+
+            // $.ajax({
+            //     type: "POST",
+            //     url: 'https://www.google.com/recaptcha/api/siteverify',
+            //     headers: {
+            //         'Access-Control-Allow-Origin': '*',
+            //         'Content-Type': 'application/json'
+            //     },
+            //     data: {
+            //         "secret": "6LdRv6MpAAAAAHknp3Ni6dnsn4HFAU9eAWH8GbJ2",
+            //         "token" : token,
+            //     },
+            //     success: function(data) {
+            //         if(data.response.success) {
+            //             window.recaptchaScore = data.response.score;
+            //             console.log('user score ' + data.response.score)
+            //         }
+            //     },
+            //     error: function() {
+            //         console.log('error while getting google recaptcha score!')
+            //     }
+            // });
 
         });
       });
@@ -58,4 +56,16 @@ function sendMail() {
     //     document.getElementById('EmailUser').focus();
     // }
 
+}
+
+async function verifyRecaptcha(token) {
+    const response = await fetch("/.netlify/functions/recaptcha?response=" + token);
+    const data = await response.json();
+    return data;
+}
+
+function validateEmail(email) {
+    // Regular expression for basic email validation
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
 }
